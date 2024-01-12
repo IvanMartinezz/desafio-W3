f8d7d0f9be46f96929b37e82c1cbae717d3388e6
Ivan Martinez <50034274+IvanMartinezz@users.noreply.github.com>
2024-01-12 17:01:15 -0300

    se implementó el  servicio GET que filtra países por nombre

--- backend/src/controllers/countryController.js
+++ backend/src/controllers/countryController.js
@@ -2,7 +2,58 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-class CountryController {}
+class CountryController {
+  async getCountries(req, res) {
+    const { value } = req.query;
+
+    const pattern = /^[a-zA-Z]+$/;
+
+    const hasInvalidCharacters = !pattern.test(value);
+
+    const hasInvalidLength = !value || value.length < 3;
+
+    if (hasInvalidCharacters) return res.status(400).json();
+
+    if (hasInvalidLength) return res.status(204).json();
+
+    try {
+      const sumTotalPopulation = await prisma.country.aggregate({
+        _sum: {
+          population: true,
+        },
+      });
+
+      const totalPopulation = Number(sumTotalPopulation._sum.population);
+
+      const countries = await prisma.country.findMany({
+        where: {
+          name: {
+            contains: value,
+            mode: "insensitive",
+          },
+        },
+        take: 5,
+      });
+
+      const hasNoMatches = countries.length === 0;
+
+      if (hasNoMatches) return res.status(404).json();
+
+      const data = countries.map((c) => ({
+        ...c,
+        population: c.population.toString(),
+        percentage_population: (
+          (Number(c.population) / totalPopulation) *
+          100
+        ).toString(),
+      }));
+      return res.status(200).json(data);
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json();
+    }
+  }
+}
 
 const countryController = new CountryController();
 module.exports = countryController;
